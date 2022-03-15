466eea5448f0ce50144555b93e8be7e1062cd038
joshkrab <joshkrab@gmail.com>
2022-03-15 23:59:21 +0200

    test01

--- #source/js/main.js
+++ #source/js/main.js
@@ -1,48 +1,76 @@
 let openP = document.querySelector('.header__links');
 let popup1 = document.querySelector('.popup1');
-openP.addEventListener('click', function () {
-   // toggle в отличии от add работает туда и обратно
-   popup1.classList.add('open');
-   body.classList.add('scroll');
-});
+let body = document.body;
+
+if (openP) {
+   openP.addEventListener('click', function () {
+      // toggle в отличии от add работает туда и обратно
+      popup1.classList.add('open');
+      body.classList.add('scroll');
+   });
+}
 
 let closeP = document.querySelector('.popup1__close');
-closeP.addEventListener('click', function () {
-   popup1.classList.remove('open');
-   body.classList.remove('scroll');
-});
+if (closeP) {
+   closeP.addEventListener('click', function () {
+      popup1.classList.remove('open');
+      body.classList.remove('scroll');
+   });
+}
 
 // Burger
 let navLIst = document.querySelector('.nav__list');
 let burger = document.querySelector('.header__burger');
 let navClose = document.querySelector('.global__close');
-let body = document.body;
 
-burger.addEventListener('click', function () {
-   navLIst.classList.add('show1');
-   navClose.classList.add('show');
-   body.classList.add('scroll');
-});
-navClose.addEventListener('click', function () {
-   navLIst.classList.remove('show1');
-   navClose.classList.remove('show');
-   body.classList.remove('scroll');
-});
+let inputFiefd = document.querySelector('.header__input');
+let linkPopFiefd = document.querySelector('.header__links');
+
+if (burger) {
+   burger.addEventListener('click', function () {
+      navLIst.classList.add('show1');
+      navClose.classList.add('show');
+      body.classList.add('scroll');
+      inputFiefd.hidden = true;
+      linkPopFiefd.hidden = true;
+      burger.classList.add('hiddenx');
+   });
+}
+
+if (navClose) {
+   navClose.addEventListener('click', function () {
+      navLIst.classList.remove('show1');
+      navClose.classList.remove('show');
+      body.classList.remove('scroll');
+      inputFiefd.hidden = false;
+      linkPopFiefd.hidden = false;
+      burger.classList.remove('hiddenx');
+   });
+}
 
 // Catalog window
 let catalogBtn = document.querySelector('.nav__show');
-let catList = document.querySelector('.catalog__list');
+let catList = document.querySelector('.catalog__list.sub2');
 let back1 = document.querySelector('.arrow-back');
 let sticky = document.querySelector('.header__sticky');
+let beforeBack = document.querySelector('.catalog__list.show2::before');
 
-catalogBtn.addEventListener('click', function () {
-   back1.classList.add('show');
-   catList.classList.add('show2');
-   sticky.classList.add('sticky-index');
-});
+if (catalogBtn) {
+   catalogBtn.addEventListener('click', function () {
+      navLIst.classList.remove('show1');
+      back1.classList.add('show');
+      catList.classList.add('show2');
+      // sticky.classList.add('sticky-index');
+      navClose.classList.remove('show');
+   });
+}
 
-back1.addEventListener('click', function () {
-   back1.classList.remove('show');
-   catList.classList.remove('show2');
-   sticky.classList.remove('sticky-index');
-});
+if (back1) {
+   back1.addEventListener('click', function () {
+      back1.classList.remove('show');
+      catList.classList.remove('show2');
+      // sticky.classList.remove('sticky-index');
+      navClose.classList.add('show');
+      navLIst.classList.add('show1');
+   });
+}
